f04514db941d6d9edb3c6c93a79b33d0956157eb
winterastrr <just.aidyn1000@gmail.com>
2026-05-17 12:30:17 -0400

    test

---
+++ Packages/fusion.lua
@@ -0,0 +1,23 @@
+local REQUIRED_MODULE = require(script.Parent._Index["plainsour_lemon@0.3.0-dev.5.8"]["lemon"])
+export type Animatable = REQUIRED_MODULE.Animatable 
+export type UsedAs<T> = REQUIRED_MODULE.UsedAs<T>
+export type Child = REQUIRED_MODULE.Child 
+export type Computed<T> = REQUIRED_MODULE.Computed<T>
+export type Contextual<T> = REQUIRED_MODULE.Contextual<T>
+export type Dependency = REQUIRED_MODULE.Dependency 
+export type Dependent = REQUIRED_MODULE.Dependent 
+export type For<KO, VO> = REQUIRED_MODULE.For<KO, VO>
+export type Observer = REQUIRED_MODULE.Observer 
+export type Hourglass = REQUIRED_MODULE.Hourglass 
+export type PropertyTable = REQUIRED_MODULE.PropertyTable 
+export type Scope<Constructors> = REQUIRED_MODULE.Scope<Constructors>
+export type ScopedObject = REQUIRED_MODULE.ScopedObject 
+export type SpecialKey = REQUIRED_MODULE.SpecialKey 
+export type Spring<T> = REQUIRED_MODULE.Spring<T>
+export type StateObject<T> = REQUIRED_MODULE.StateObject<T>
+export type Task = REQUIRED_MODULE.Task 
+export type Tween<T> = REQUIRED_MODULE.Tween<T>
+export type Use = REQUIRED_MODULE.Use 
+export type Value<T, S = T> = REQUIRED_MODULE.Value<T, S >
+export type Version = REQUIRED_MODULE.Version 
+return REQUIRED_MODULE
